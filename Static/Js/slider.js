ef3bcf9804584a06f3004dbb636f9d4d6d6880ca
tanvir321110 <tanvirahmed321110@gmail.com>
2025-01-08 10:31:05 +0600

    testimonila section ok

--- Static/Js/slider.js
+++ Static/Js/slider.js
@@ -20,3 +20,65 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 });
+
+
+
+
+
+// top sell slider
+document.addEventListener("DOMContentLoaded", function () {
+    const secondSwiper = new Swiper('.top-sell-slider', {
+        slidesPerView: 1, // Number of slides to show
+        spaceBetween: 20, // Space between slides
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+        },
+    });
+});
+
+
+
+
+
+// Testimonial Slider
+const testimonialSlider = new Swiper('.testimonial-slider', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 0,
+    loop: true,
+    speed: 2000,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        350: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
