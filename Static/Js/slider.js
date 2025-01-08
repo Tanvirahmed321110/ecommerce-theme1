3358129a85306ba7eebaa23f0976414dfb04ef45
tanvir321110 <tanvirahmed321110@gmail.com>
2025-01-09 01:23:02 +0600

    home page 99% ok

--- Static/Js/slider.js
+++ Static/Js/slider.js
@@ -1,66 +1,32 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-    var swiper = new Swiper(".mySwiper", {
-        effect: "cube",
-        grabCursor: true,
-        loop: true,
-        speed: 2000,
-        cubeEffect: {
-            shadow: true,
-            slideShadows: true,
-            shadowOffset: 20,
-            shadowScale: 0.94,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        autoplay: {
-            delay: 3000,
-        },
-    });
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    speed: 2000,
+    cubeEffect: {
+        shadow: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
 });
 
 
 
 
 
-// top sell slider
-document.addEventListener("DOMContentLoaded", function () {
-    const secondSwiper = new Swiper('.top-sell-slider', {
-        slidesPerView: 1, // Number of slides to show
-        spaceBetween: 20, // Space between slides
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        breakpoints: {
-            640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-            },
-            1024: {
-                slidesPerView: 4,
-                spaceBetween: 40,
-            },
-        },
-    });
-});
 
 
 
 
 
 // Testimonial Slider
-const testimonialSlider = new Swiper('.testimonial-slider', {
+var testimonialSlider = new Swiper('.testimonial-slider', {
     loop: true,
     slidesPerView: 3,
     spaceBetween: 0,
